9de1fb36b2b03ad24b43480cde7435d48960c353
jianrui <dingjianrui@outlook.com>
2016-06-23 22:44:05 +0800

    update

--- reducejoin/src/main/java/djr/hadoop/test/AccountRecordMapper.java
+++ reducejoin/src/main/java/djr/hadoop/test/AccountRecordMapper.java
@@ -11,6 +11,6 @@ public class AccountRecordMapper extends Mapper<Object, Text, Text, Text> {
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
 		String record = value.toString();
 		String parts[] = record.split("\t");
-		context.write(new Text(parts[0]), new Text("account\t" + parts[1]));
+		context.write(new Text(parts[0]), new Text("accounts\t" + parts[1]));
 	}
 }
